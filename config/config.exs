493fcc3d2641f9bd92f83e3416be44d2e7e82b24
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-12-26 10:33:17 -0600

    working on prod stuff

--- config/config.exs
+++ config/config.exs
@@ -28,8 +28,8 @@ config :ueberauth, Ueberauth,
     google: {Ueberauth.Strategy.Google, [default_scope: "emails profile plus.me"]}
   ]
 config :ueberauth, Ueberauth.Strategy.Google.OAuth,
-  client_id: System.get_env("GOOGLE_CLIENT_ID"),
-  client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
+  client_id: "${GITHUB_CLIENT_ID}",
+  client_secret: "${GITHUB_SECRET_ID}"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
